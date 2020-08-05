25570ccbdc4c11dd7382bd237528b82c38abc7ef
FaizaNawaz017 <64863731+FaizaNawaz017@users.noreply.github.com>
2020-08-05 15:37:45 +0500

    The final Exam Containing Assignmet 4a,4b,5a and 5b is done .

---
+++ Assets/BackButton1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackButton1 : MonoBehaviour
+{
+    public void backtoMainMenu()
+    {
+        SceneManager.LoadScene("Palindrome_menu");
+    }
+}
+
---
+++ Assets/Scripts/StringAttachement33.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StringAttachement33 : MonoBehaviour
+{
+
+    public Text nameLable;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 namePose = Camera.main.WorldToScreenPoint(this.transform.position);
+        nameLable.transform.position = namePose;
+    }
+}
